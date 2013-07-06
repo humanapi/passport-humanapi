7e0f9b57539dc562fcff6a44b2942d27163282eb
Ola Wiberg <olawiberg@gmail.com>
2013-07-05 19:44:28 -0700

    Initial commit of the HumanAPI passport module.

---
+++ README.md
@@ -0,0 +1,68 @@
+# Passport-HumanAPI
+
+[Passport](https://github.com/jaredhanson/passport) strategy for authenticating
+with [HumanAPI's Personal Health Data API](https://api.humanapi.co) using the OAuth 2.0 API.
+
+## Installation
+
+    $ npm install passport-humanapi
+
+## Usage
+
+#### Configure Strategy
+
+The HumanAPI Personal Health Data API authentication strategy authenticates users using a HumanAPI Personal Health Data API account and
+OAuth tokens.  The strategy requires a `verify` callback, which accepts these credentials and calls `done` providing a user, as well as `options` specifying a consumer key, consumer secret, and callback URL.
+
+    passport.use(new HumanApiStrategy({
+        consumerKey: HUMANAPI_CONSUMER_KEY,
+        consumerSecret: HUMANAPI_CONSUMER_SECRET,
+        callbackURL: "http://127.0.0.1:3000/auth/humanapi/callback",
+        scope: "profile "
+      },
+      function(token, tokenSecret, profile, done) {
+        User.findOrCreate({ userId: profile.id }, function (err, user) {
+          return done(err, user);
+        });
+      }
+    ));
+
+#### Authenticate Requests
+
+Use `passport.authenticate()`, specifying the `'humanapi'` strategy, to
+authenticate requests.
+
+For example, as route middleware in an [Express](http://expressjs.com/)
+application:
+
+    app.get('/auth/humanapi',
+      passport.authenticate('humanapi'),
+      function(req, res){
+        // The request will be redirected to HumanAPI Personal Health Data API for authentication, so this
+        // function will not be called.
+      });
+
+    app.get('/auth/humanapi/callback',
+      passport.authenticate('humanapi', { failureRedirect: '/login' }),
+      function(req, res) {
+        // Successful authentication, redirect home.
+        res.redirect('/');
+      });
+
+## Credits
+  - [Ola Wiberg](https://github.com/olawiberg)
+
+## Thanks
+  - [Jared Hanson](https://github.com/jaredhanson)
+
+## License
+
+(The MIT License)
+
+Copyright (c) 2012 Ola Wiberg
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ examples/login/app.js
@@ -0,0 +1,124 @@
+var express = require('express')
+  , passport = require('passport')
+  , util = require('util')
+  // , HumanApiStrategy = require('passport-humanapi').Strategy;
+  , HumanApiStrategy = require('../../lib/passport-humanapi').Strategy;
+
+var HUMANAPI_APP_ID = "--insert-humanapi-app-id-here--"
+var HUMANAPI_APP_SECRET = "--insert-humanapi-app-secret-here--";
+
+
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.  However, since this example does not
+//   have a database of user records, the complete HumanAPI Personal Health Data API profile is serialized
+//   and deserialized.
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+
+// Use the HumanApiStrategy within Passport.
+//   Strategies in Passport require a `verify` function, which accept
+//   credentials (in this case, an accessToken, refreshToken, and HumanAPI
+//   profile), and invoke a callback with a user object.
+passport.use(new HumanApiStrategy({
+    clientID: HUMANAPI_APP_ID,
+    clientSecret: HUMANAPI_APP_SECRET,
+    callbackURL: "http://localhost:3000/auth/humanapi/callback",
+    scope: "heart_rate bmi bodyfat"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+      
+      // To keep the example simple, the user's HumanAPI Personal Health Data API profile is returned to
+      // represent the logged-in user.  In a typical application, you would want
+      // to associate the HumanAPI Personal Health Data API account with a user record in your database,
+      // and return that user instead.
+      return done(null, profile);
+    });
+  }
+));
+
+
+
+
+var app = express.createServer();
+
+// configure Express
+app.configure(function() {
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.logger());
+  app.use(express.cookieParser());
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.session({ secret: 'congrats daltonc' }));
+  // Initialize Passport!  Also use passport.session() middleware, to support
+  // persistent login sessions (recommended).
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+
+app.get('/', function(req, res){
+  res.render('index', { user: req.user });
+});
+
+app.get('/account', ensureAuthenticated, function(req, res){
+  res.render('account', { user: req.user });
+});
+
+app.get('/login', function(req, res){
+  res.render('login', { user: req.user });
+});
+
+// GET /auth/humanapi
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  The first step in HumanAPI Personal Health Data API authentication will involve
+//   redirecting the user to humanapi.com.  After authorization, HumanAPI Personal Health Data API will
+//   redirect the user back to this application at /auth/humanapi/callback
+app.get('/auth/humanapi',
+  passport.authenticate('humanapi'),
+  function(req, res){
+    // The request will be redirected to HumanAPI Personal Health Data API for authentication, so this
+    // function will not be called.
+  });
+
+// GET /auth/humanapi/callback
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  If authentication fails, the user will be redirected back to the
+//   login page.  Otherwise, the primary route function function will be called,
+//   which, in this example, will redirect the user to the home page.
+app.get('/auth/humanapi/callback', 
+  passport.authenticate('humanapi', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+app.listen(3000);
+
+
+// Simple route middleware to ensure user is authenticated.
+//   Use this route middleware on any resource that needs to be protected.  If
+//   the request is authenticated (typically via a persistent login session),
+//   the request will proceed.  Otherwise, the user will be redirected to the
+//   login page.
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login')
+}
